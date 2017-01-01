614f3907fa3357101b1811cd7358c82c99e0ce3d
Arnun <it_531413016@hotmail.com>
2017-01-01 16:18:46 +0700

    remove old file (T_T) bye bye

--- old_stuff/Insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-include (__DIR__ . '/include.php');
-
-$book = new \RB\Model\Book();
-$book->setName('book'.rand(1,1000));
-$book->setPrice(rand(1, 1000));
-
-echo "<p>Inserted! got ID: ".$book->insertAction()."</p>";
--- old_stuff/example/src/manualload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-foreach (glob(__DIR__."/Intern/UI/Shortcode/*.php") as $filename)
-{
-    include $filename;
-}
